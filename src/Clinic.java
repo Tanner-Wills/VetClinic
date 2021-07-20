a924f3d32299f8a5b952dc4f0108a292a6f24719
Tanner-Wills <tannerwills1@gmail.com>
2021-07-20 15:40:03 -0400

    Debugging Clinic method for reading .csv files

---
+++ src/Clinic.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.FileReader;
+import java.util.List;
+
+public class Clinic {
+
+    //Variables
+    File patientFile;
+    private int day;
+
+
+    //Constructor
+    public Clinic(File file) {
+        this.patientFile = file;
+
+    }
+
+    public Clinic(String filename) {
+        this(new File(filename));
+    }
+
+    //Clinic Methods
+    public String nextDay(File f) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(f));
+        scanner.useDelimiter(",");
+        String appoint = "Consultation for " + scanner.next() + " the " + scanner.next() + " at " + scanner.next() + ".\nWhat is the health of " + scanner.next() + "?\n";
+        System.out.println(appoint);
+        scanner.close();
+
+        return appoint;
+    }
+    public String nextDay(String filename){
+        Scanner scanner = new Scanner("C:\\Users\\dvuf0zn\\IdeaProjects\\VetClinic\\src\\Appointments.csv");
+        scanner.useDelimiter(",");
+        String appoint = "Consultation for " + scanner.next() + " the " + scanner.next() + " at " + scanner.next() + ".\nWhat is the health of " + scanner.next() + "?\n";
+        System.out.println(appoint);
+        scanner.close();
+        BufferedReader
+        return appoint;
+    }
+}
--- src/TestClinic.java
+++ src/TestClinic.java
@@ -1,13 +1,8 @@
 public class TestClinic {
     public static void main(String[] args) {
 
-    Dog poopsie = new Dog("poopsie", 10, 3, 0);
-    Cat cowboy = new Cat("Cowboy", 10, 10, 10);
-    Cat johm = new Cat("John", 10, 10, 10);
-        poopsie.speak();
-        System.out.println(poopsie.getDroolRate());
+        Clinic happy = new Clinic("C:\\Users\\dvuf0zn\\IdeaProjects\\VetClinic\\src\\Appointments.csv");
+        happy.nextDay("C:\\Users\\dvuf0zn\\IdeaProjects\\VetClinic\\src\\Appointments.csv");
 
-        cowboy.speak();
-        System.out.println(johm.equals(cowboy));
     }
 }
