eb013594ef0f7467639f37cc636fdf41d92b02f1
Tanner-Wills <tannerwills1@gmail.com>
2021-07-21 13:39:47 -0400

    Updated military time conversion method

--- src/Clinic.java
+++ src/Clinic.java
@@ -1,9 +1,8 @@
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.io.FileReader;
-import java.util.List;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Clinic {
 
@@ -37,31 +36,60 @@ public class Clinic {
     public String nextDay(String filename) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File(filename));
         String patient = null;
+
         while(scanner.hasNextLine()){
             patient = scanner.nextLine();
-
             String[] token = patient.split(",");
             String name = token[0];
             String typeOfPet = token[1];
-            if(typeOfPet == "Cat"){
+            if(typeOfPet == "Dog"){
                 String droolRate = token[2];
                 //double droolRate = Double.parseDouble(token[2]);
-            } else {
+            } else if (typeOfPet == "Cat") {
                 String miceCaught = token[2];
                 //int miceCaught = Integer.parseInt(token[2]);
             }
-            String time = token[3];
-            
-            String appoint = "Consultation for " + name + " the " + typeOfPet + " at " + time + ".\nWhat is the health of " + name + "?\n";
+            String military = token[3];
+
+            String appoint = "Consultation for " + name + " the " + typeOfPet + " at " + miltime(military) + ".\nWhat is the health of " + name + "?\n";
             System.out.println(appoint);
         }
-
-
-
-        //scanner.useDelimiter(",");
-        //String appoint = "Consultation for " + scanner.next() + " the " + scanner.next() + " at " + scanner.next() + ".\nWhat is the health of " + scanner.next() + "?\n";
         scanner.close();
-
         return patient;
     }
+
+
+    private String miltime(String military){
+        String hours;
+        String minutes;
+        int hour0 = Integer.parseInt(Character.toString(military.charAt(0)));
+        int hour1 = Integer.parseInt(Character.toString(military.charAt(1)));
+        String hour0str = Character.toString(military.charAt(0));
+        String hour1str = Character.toString(military.charAt(1));
+        String hour2str = Character.toString(military.charAt(2));
+        String hour3str = Character.toString(military.charAt(3));
+
+            //for times greater than 1200
+        if(hour0 > 0 && hour1 > 2){
+            hours = String.valueOf(Integer.parseInt((hour0str + hour1str)) - 12);
+            minutes = hour2str + hour3str + " pm";
+
+            //for times between 1200 and 1300
+        } else if (hour0 > 0 && hour1 == 2){
+            hours = hour0str + hour1str;
+            minutes = hour2str + hour3str + " pm";
+
+            //for times between 1000 and 1159
+        } else if (hour0 > 0 && hour1 < 2){
+            hours = hour0str + hour1str;
+            minutes = hour2str + hour3str + " am";
+
+            //for times less than 1000
+        } else {
+            hours = hour1str;
+            minutes = hour2str + hour3str + " am";
+        }
+        return military = hours + ":" + minutes;
+    }
+
 }
