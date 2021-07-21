0b25bdb0f912e4b0d39835b93dcd299044d3f8be
Tanner-Wills <tannerwills1@gmail.com>
2021-07-21 09:30:31 -0400

    Updated scanner for clinic class

--- src/Clinic.java
+++ src/Clinic.java
@@ -20,25 +20,48 @@ public class Clinic {
 
     public Clinic(String filename) {
         this(new File(filename));
+
     }
 
     //Clinic Methods
     public String nextDay(File f) throws FileNotFoundException {
-        Scanner scanner = new Scanner("C:\\Users\\dvuf0zn\\IdeaProjects\\VetClinic\\Appointments.csv");
+        Scanner scanner = new Scanner(new File(String.valueOf(f)));
         scanner.useDelimiter(",");
+
         String appoint = "Consultation for " + scanner.next() + " the " + scanner.next() + " at " + scanner.next() + ".\nWhat is the health of " + scanner.next() + "?\n";
         System.out.println(appoint);
         scanner.close();
 
         return appoint;
     }
-    public String nextDay(String filename){
-        Scanner scanner = new Scanner("C:\\Users\\dvuf0zn\\IdeaProjects\\VetClinic\\Appointments.csv");
-        scanner.useDelimiter(",");
-        String appoint = "Consultation for " + scanner.next() + " the " + scanner.next() + " at " + scanner.next() + ".\nWhat is the health of " + scanner.next() + "?\n";
-        System.out.println(appoint);
+    public String nextDay(String filename) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(filename));
+        String patient = null;
+        while(scanner.hasNextLine()){
+            patient = scanner.nextLine();
+
+            String[] token = patient.split(",");
+            String name = token[0];
+            String typeOfPet = token[1];
+            if(typeOfPet == "Cat"){
+                String droolRate = token[2];
+                //double droolRate = Double.parseDouble(token[2]);
+            } else {
+                String miceCaught = token[2];
+                //int miceCaught = Integer.parseInt(token[2]);
+            }
+            String time = token[3];
+            
+            String appoint = "Consultation for " + name + " the " + typeOfPet + " at " + time + ".\nWhat is the health of " + name + "?\n";
+            System.out.println(appoint);
+        }
+
+
+
+        //scanner.useDelimiter(",");
+        //String appoint = "Consultation for " + scanner.next() + " the " + scanner.next() + " at " + scanner.next() + ".\nWhat is the health of " + scanner.next() + "?\n";
         scanner.close();
 
-        return appoint;
+        return patient;
     }
 }
--- src/TestClinic.java
+++ src/TestClinic.java
@@ -1,5 +1,7 @@
+import java.io.FileNotFoundException;
+
 public class TestClinic {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         Clinic happy = new Clinic("C:\\Users\\dvuf0zn\\IdeaProjects\\VetClinic\\Appointments.csv");
         happy.nextDay("C:\\Users\\dvuf0zn\\IdeaProjects\\VetClinic\\Appointments.csv");
